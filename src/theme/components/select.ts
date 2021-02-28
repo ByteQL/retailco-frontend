00bd26d8ddd67f86b919b862195a4cebbf3a89c3
habib-byteql <habib@byteql.com>
2021-02-28 06:45:04 +0100

    fix form validation

---
+++ src/theme/components/select.ts
@@ -0,0 +1,30 @@
+const Select = {
+  baseStyle: {
+    field: {
+      fontSize: '1.8rem',
+    },
+  },
+  sizes: {
+    sm: {
+      field: {
+        height: '3.5rem',
+      },
+    },
+    lg: {
+      field: {
+        height: '5rem !important',
+        fontSize: 'md',
+      },
+    },
+    xl: {
+      field: {
+        height: '6rem',
+      },
+    },
+  },
+  defaultProps: {
+    size: 'lg',
+  },
+};
+
+export default Select;
