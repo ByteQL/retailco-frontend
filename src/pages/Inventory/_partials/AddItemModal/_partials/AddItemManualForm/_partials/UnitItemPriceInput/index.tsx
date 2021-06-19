e8e26dc61e8d0b5b495cc041602041d7cea4e4f1
habib-byteql <habib@byeql.com>
2021-06-19 04:24:01 +0100

    UI changes

--- src/pages/Inventory/_partials/AddItemModal/_partials/AddItemManualForm/_partials/UnitItemPriceInput/index.tsx
+++ src/pages/Inventory/_partials/AddItemModal/_partials/AddItemManualForm/_partials/UnitItemPriceInput/index.tsx
@@ -10,40 +10,59 @@ import {
   SimpleGrid,
 } from '@chakra-ui/react';
 
-const UnitItemPriceInput = ({ item, i, handleChangeItemUnit }) => (
-  <Box
-    key={i}
-    p="1.5rem 0"
-    borderTop="1px solid"
-    borderTopColor="grey.200"
-    mt="0 !important"
-  >
-    <Heading as="h3" size="sm">
-      Unit {i + 1}&nbsp;&nbsp;
-      <Box as="span" fontWeight={400}>
-        Set price, quantity and discount for this unit
-      </Box>
-    </Heading>
-    <SimpleGrid mt="3rem" columns={2} spacing={10}>
-      <FormControl id={`unit-price-${i}`}>
-        <FormLabel>Item Price</FormLabel>
-        <Input
-          size="md"
-          placeholder="0.00"
-          value={item.price}
-          onChange={(e) => handleChangeItemUnit(i, 'price', e.target.value)}
-        />
-      </FormControl>
-      <FormControl id={`unit-quantity-${i}`}>
-        <FormLabel>Item Quantity</FormLabel>
-        <Input
-          size="md"
-          placeholder="0"
-          value={item.quantity}
-          onChange={(e) => handleChangeItemUnit(i, 'quantity', e.target.value)}
-        />
-      </FormControl>
-    </SimpleGrid>
-  </Box>
-);
+const UnitItemPriceInput = ({
+  item,
+  i,
+  handleChangeItemUnit,
+  isMultipleUnitsChecked,
+}) => {
+  console.log(item);
+
+  return (
+    <Box key={i} w={{ xl: '65rem' }}>
+      {isMultipleUnitsChecked && (
+        <Heading as="h3" size="sm">
+          <Box as="span" fontWeight={400} fontStyle="italic" fontSize="1.2rem">
+            Set price, quantity and discount for this unit
+          </Box>
+        </Heading>
+      )}
+      <SimpleGrid mt="3rem" columns={3} spacing={10}>
+        <FormControl id={`quantity-${i}`}>
+          <FormLabel>Product Quantity</FormLabel>
+          <Input
+            size="md"
+            placeholder="0.00"
+            value={item.quantity}
+            onChange={(e) =>
+              handleChangeItemUnit(i, 'quantity', e.target.value)
+            }
+          />
+        </FormControl>
+        <FormControl id={`cost_price-${i}`}>
+          <FormLabel>Cost price</FormLabel>
+          <Input
+            size="md"
+            placeholder="0"
+            value={item.cost_price}
+            onChange={(e) =>
+              handleChangeItemUnit(i, 'cost_price', e.target.value)
+            }
+          />
+        </FormControl>
+        <FormControl id={`selling_price-${i}`}>
+          <FormLabel>Selling price</FormLabel>
+          <Input
+            size="md"
+            placeholder="0"
+            value={item.selling_price}
+            onChange={(e) =>
+              handleChangeItemUnit(i, 'selling_price', e.target.value)
+            }
+          />
+        </FormControl>
+      </SimpleGrid>
+    </Box>
+  );
+};
 export default UnitItemPriceInput;
